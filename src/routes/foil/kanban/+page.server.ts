db7cd224e9fc4c857f5f75fd5fc1148c75dcc661
Julian Passebecq <64646018+julian-passebecq@users.noreply.github.com>
2026-09-23 18:02:54 +0200

    feat(foil): load authoritative PM Kanban

---
+++ src/routes/foil/kanban/+page.server.ts
@@ -0,0 +1,6 @@
+import { executeReport } from "$lib/server/reportEngine";
+import type { PageServerLoad } from "./$types";
+
+export const load: PageServerLoad = async () => ({
+	report: await executeReport("FOIL_KANBAN")
+});
