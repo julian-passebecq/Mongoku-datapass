3763f6992dbe2b1496fcd64698a1903b1bd7cd9d
Julian Passebecq <64646018+julian-passebecq@users.noreply.github.com>
2026-09-23 15:02:15 +0200

    build(vercel): select Vercel adapter on hosted builds

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,22 @@
-import adapter from "@sveltejs/adapter-node";
+import nodeAdapter from "@sveltejs/adapter-node";
+import vercelAdapter from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+
+const isVercel = process.env.VERCEL === "1";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({
-			out: "build",
-			envPrefix: "MONGOKU_SERVER_",
-		}),
+		adapter: isVercel
+			? vercelAdapter({
+					runtime: "nodejs22.x",
+				})
+			: nodeAdapter({
+					out: "build",
+					envPrefix: "MONGOKU_SERVER_",
+				}),
 		paths: {
 			base: process.env.BASE_PATH || "",
 		},
