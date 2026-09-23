5f2d8520744099113f3b81a8644a4dd8d55f6715
Julian Passebecq <64646018+julian-passebecq@users.noreply.github.com>
2026-09-23 13:22:27 +0200

    feat(datapass): make control dashboard the landing page

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,5 @@
-import { base } from "$app/paths";
-import { redirect } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async () => {
-	redirect(307, `${base}/servers`);
+	return {};
 };
