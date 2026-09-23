8c889a22de83e0dc2583f9de6bd7f0b022f1ce8a
Julian Passebecq <64646018+julian-passebecq@users.noreply.github.com>
2026-09-23 13:37:54 +0200

    feat(datapass): load canonical workspace in root layout

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,12 +1,16 @@
 import { env } from "$env/dynamic/private";
+import { loadControlWorkspace } from "$lib/server/datapassControl";
 import { getOAuthConfig } from "$lib/server/oauth";
 import type { LayoutServerLoad } from "./$types";
 
 export const load: LayoutServerLoad = async (event) => {
+	const controlWorkspace = await loadControlWorkspace();
+
 	return {
 		readOnly: env.MONGOKU_READ_ONLY_MODE === "true",
 		serverOrigin: event.url.origin,
 		oauthEnabled: !!(await getOAuthConfig()),
 		user: event.locals.user,
+		controlWorkspace
 	};
 };
